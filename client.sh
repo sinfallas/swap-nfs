ebd132c9bee0a1f8747bfb43e90f5dccc482b6d2
Jose Perez <sinfallas@users.noreply.github.com>
2015-02-17 17:19:29 -0430

    Update client.sh

--- client.sh
+++ client.sh
@@ -3,6 +3,7 @@ if [ $USER != root ]; then
 echo "Error: must be root"
 exit 1
 fi
+apt update
 apt -y install nfs-common
 mkdir /home/user/swap
 echo "192.168.1.1:/swap /swap nfs user,exec" >> /etc/fstab
